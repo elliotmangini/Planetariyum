962beb9440ab436e3727666e13f6bad225b17e00
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-29 01:36:41 -0600

    set up browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+import './StyleSheets/App.css';
+
+import Landing from './Components/Landing'
 
 export default function App() {
 
   return (
-
     <>
-    <h1>Test</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </>
-
   );
 }
---
+++ client/src/Components/Landing.js
@@ -0,0 +1,12 @@
+
+
+
+
+export default function X () {
+
+    return (
+        <>
+            <>landing</>
+        </>
+    )
+}
