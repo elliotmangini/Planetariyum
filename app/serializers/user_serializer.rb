3f87f59c4917858df2d2dd0fe4e542b1b05ca875
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-03 10:45:11 -0600

    optimized user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :bio, :email, :default_timezone, :twitch_username, :password_digest, :avatar_url
+  attributes :id, :username, :bio, :email, :default_timezone, :twitch_username, :avatar_url
 end
