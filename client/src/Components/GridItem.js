5f2e61726fe8efd48ffe1022dcc536ceaffab1b1
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-07 14:05:37 -0700

    improvements to Home Page

--- client/src/Components/GridItem.js
+++ client/src/Components/GridItem.js
@@ -7,7 +7,12 @@ export default function GridItem ({ item }) {
 
     return (
                 <div className={item.kind === "large" ? style.grid_box_span_2 : style.grid_box}>
+                    <div className={style.publication_container}>
+                    
                     <img className={style.cover_image} src={item.cover_url} ></img>
+                    <div className={style.publication_title}>{item.title}</div>
+                    
+                    </div>
                 </div>
     )
 }
