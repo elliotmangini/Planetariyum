dabd16a9eecf719d1ced1118c69686dd374cdfc2
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-10 15:16:58 -0700

    got audio playing

--- client/src/Components/AudioPlayer.js
+++ client/src/Components/AudioPlayer.js
@@ -2,11 +2,39 @@
 
 
 
-export default function AudioPlayer ({ sound , action }) {
+export default function AudioPlayer ({ setDuration , sound , action , clearAction }) {
+
+    const audio = new Audio(sound)
 
     function play() {
-        new Audio(sound).play();
+        audio.play();
     }
+
+    function pause() {
+        audio.pause();
+    }
+
+
+    function functionSelector (action) {
+        switch(action) {
+            case "play":
+              play();
+              break;
+            case "pause":
+              pause();
+              break;
+            default:
+              console.log("no matching audio function");
+          }
+    }
+
+    audio.onloadedmetadata = () => {
+        clearAction("");
+        setDuration(audio.duration);
+        functionSelector(action);
+    }
+
+    // console.log(audio)
 
     return (
         <>
--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,11 +1,13 @@
 import style from '../StyleSheets/Game.module.css'
 // import { useState } from 'react';
 import AudioPlayer from './AudioPlayer';
-
+import {useState} from 'react';
 
 
 export default function Card ({ nft, selectedCard, isTurnEnding, lastSelected, handleSelect }) {
-    console.log(nft)
+    const [ audioAction , setAudioAction ] = useState("");
+    const [ duration , setDuration ] = useState(100000);
+    // console.log(nft)
 
     // add set down to selected card only
 
@@ -14,13 +16,26 @@ export default function Card ({ nft, selectedCard, isTurnEnding, lastSelected, h
     //     return style.add_flip_facedown;
     // }
 
+    function handleClick () {
+        console.log("hit");
+        setAudioAction("play");
+        const timer = setTimeout(() => {
+            if (!(selectedCard.id === nft.id)) {
+                handleSelect(nft)
+                console.log("done!")
+            }
+          }, 1
+        );
+        return () => clearTimeout(timer);
+    }
+
 
     return (
         <>  
                 <div className={`${style.flip_card}`}>
                     <div className={`${style.flip_card_inner} ${!selectedCard.id && !isTurnEnding ? style.add_flip_reveal : style.no_flip} ${isTurnEnding ? style.add_flip_facedown : null}`}>
                         <div className={`${style.flip_card_front}`}>
-                            <img onClick={() => handleSelect(nft)} className={`${style.image_sizing_selectable} ${selectedCard.id === nft.id ? style.selected_unhoverable : style.unselected_hoverable} ${lastSelected.id === nft.id ? style.set_down : null } ${selectedCard.id === nft.id && isTurnEnding ? style.set_selected_down : null}`} src={nft.card.art_url} alt="Avatar" />
+                            <img onClick={() => handleClick()} className={`${style.image_sizing_selectable} ${selectedCard.id === nft.id ? style.selected_unhoverable : style.unselected_hoverable} ${lastSelected.id === nft.id ? style.set_down : null } ${selectedCard.id === nft.id && isTurnEnding ? style.set_selected_down : null}`} src={nft.card.art_url} alt="Avatar" />
                         </div>
                         <div className={`${style.flip_card_back}`}>
                             <img className={`${style.image_sizing_selectable}`} src={nft.card.collection_card_back_url} alt="Avatar" />
@@ -28,7 +43,7 @@ export default function Card ({ nft, selectedCard, isTurnEnding, lastSelected, h
                     </div>
                 </div>
 
-                <AudioPlayer sound={nft.card.asset_url} />
+                {audioAction ? <AudioPlayer setDuration={setDuration} action={audioAction} clearAction={setAudioAction}  sound={nft.card.asset_url} /> : null }
         </>
     )
 }
