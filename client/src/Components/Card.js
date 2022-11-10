a302a02b0af9ad2c305f73d1974dfc21d325e7cb
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-10 16:02:42 -0700

    another bug fix

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -29,7 +29,7 @@ export default function Card ({ nft, selectedCard, isTurnEnding, lastSelected, h
                 <div className={`${style.flip_card}`}>
                     <div className={`${style.flip_card_inner} ${!selectedCard.id && !isTurnEnding ? style.add_flip_reveal : style.no_flip} ${isTurnEnding ? style.add_flip_facedown : null}`}>
                         <div className={`${style.flip_card_front}`}>
-                            <img onClick={() => handleClick()} className={`${style.image_sizing_selectable} ${selectedCard.id === nft.id ? style.selected_unhoverable : style.unselected_hoverable} ${lastSelected.id === nft.id ? style.set_down : null } ${selectedCard.id === nft.id && isTurnEnding ? style.set_selected_down : null}`} src={nft.card.art_url} alt="Avatar" />
+                            <img onClick={() => handleClick()} className={`${style.image_sizing_selectable} ${selectedCard.id === nft.id ? style.selected_unhoverable : style.unselected_hoverable} ${lastSelected.id === nft.id && !isTurnEnding ? style.set_down : null } ${selectedCard.id === nft.id && isTurnEnding ? style.set_selected_down : null}`} src={nft.card.art_url} alt="Avatar" />
                         </div>
                         <div className={`${style.flip_card_back}`}>
                             <img className={`${style.image_sizing_selectable}`} src={nft.card.collection_card_back_url} alt="Avatar" />
