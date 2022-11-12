4271ff10ca43cc3245d7896dde07eda12e6565b6
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-11 18:18:55 -0700

    check check

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -8,7 +8,21 @@ import SignUp3 from "./SignUp3";
 
 
 export default function SignUp ({ user, setUser, isLogout }) {
-  const [sequence , setSequence ] = useState(1);
+  const [sequence , setSequence ] = useState(0);
+  console.log({ sequence })
+  console.log("user" + user)
+  // console.log("url save" + user.avatar_url)
+
+
+  // refresh helper
+  useEffect(() => {
+    console.log("useEffect firing")
+    if (user) {
+      if (!user.avatar_url) {
+        setSequence(2);
+      }
+    }
+  }, [user]);
 
   return (
     <>
@@ -16,10 +30,11 @@ export default function SignUp ({ user, setUser, isLogout }) {
         
         {/* BACKWARD PATH */}
         {/* Encourage Avatar Upload, Allow Refresh, Make Unusable if steps completed */}
-        { user ? <>{ !user.avatar_url || sequence == 2 ? <SignUp2 setSequence={setSequence} user={user} setUser={setUser} /> : null }</> : null}
+        { user ? <>{ !user.avatar_url || sequence === 2 ? <SignUp2 setSequence={setSequence} user={user} setUser={setUser} /> : null }</> : null}
 
 
-        { sequence == 3 ? <SignUp3 setSequence={setSequence} user={user} setUser={setUser} /> : null}
+        {/* REFRESH AFTER UPLOADED, OR SKIP, OR CONFIRM */}
+        { user ? <>{ user.avatar_url && (sequence !== 2) || sequence === 3 ? <SignUp3 setSequence={setSequence} user={user} setUser={setUser} /> : null }</> : null}
 
         
 
