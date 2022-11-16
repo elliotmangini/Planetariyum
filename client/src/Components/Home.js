25fceb9fc953401ac773b38ebecb1a67875e8b2c
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-16 13:22:22 -0700

    added autoscroll and recolored scroll bar

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import style from '../StyleSheets/Home.module.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import GridItem from './GridItem';
 
@@ -9,6 +9,7 @@ import Robot1 from '../Assets/robots/robot6.png';
 
 
 export default function Home () {
+    const ref = useRef(null);
 
       // GET STUFF FOR HOME
   const [ publications , setPublications ] = useState([]);
@@ -25,11 +26,16 @@ export default function Home () {
         )
     })
 
+    function scrollToTop () {
+        ref.current?.scrollIntoView({behavior: 'smooth'});
+        console.log("scrolling");
+    }
+
 
 
     return (
         <div className='display-block'>
-            <div className={`${style.grid_container} halfsecond-lazyload`}>
+            <div ref={ref} className={`${style.grid_container} halfsecond-lazyload`}>
                 <div className={style.grid}>
                     {gridItems}
                 </div>
@@ -39,14 +45,24 @@ export default function Home () {
                     <img className={`${style.article_image}`} src={Robot1} />
                 </div>
 
-                <div className={`dev-box ${style.article_wrapper}`}>
-                    <h2>Welcome</h2>
-                    <p style={{width: "90%", margin: "auto"}}>Welcome to our social network for artists! We are excited to have you here and hope that you will find our community to be a supportive and creative space. Our site is filled with features that will help you connect with other artists, showcase your work, and get inspired. Here are a few things you can do to get started:</p>
-                    <p style={{textAlign: "center"}}>-Explore the work of other artists in our community.</p>
-                    <p style={{textAlign: "center"}}>-Create a profile and start sharing your own art.</p>
-                    <p style={{textAlign: "center"}}>-Join conversations in our forums and groups.</p>
-                    <p style={{textAlign: "center"}}>-Check out our calendar of upcoming events.</p>
-                    <h3 style={{textAlign: "center"}}>We can't wait to see what you create!</h3>
+
+                <div className={`dev-box ${style.article_text_wrapper}`}>
+                    <h2>Welcome To<br></br><span>Planetariyum</span></h2>
+                    <div className={`${style.article_body_wrapper}`}>
+                        <p style={{width: "90%", margin: "auto"}}>Welcome to our social network for artists! We are excited to have you here and hope that you will find our community to be a supportive and creative space. Our site is filled with features that will help you connect with other artists, showcase your work, and get inspired. Here are a few things you can do to get started:</p>
+                        <p style={{textAlign: "center"}}>-Explore the work of other artists in our community.</p>
+                        <p style={{textAlign: "center"}}>-Create a profile and start sharing your own art.</p>
+                        <p style={{textAlign: "center"}}>-Join conversations in our forums and groups.</p>
+                        <p style={{textAlign: "center"}}>-Check out our calendar of upcoming events.</p>
+                        <h3 style={{textAlign: "center"}}><span>We can't wait to see what you create!</span></h3>
+                    </div>
+                </div>
+                <div className={style.scroll_to_top_container} onClick={scrollToTop}>
+                    <div></div>
+                    <div className={style.like_btn}></div>
+                    <div className={style.visit_btn}></div>
+                    <div className={style.scroll_to_top_btn}></div>
+                    <div></div>
                 </div>
             </div>
         </div>
