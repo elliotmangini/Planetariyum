cb9cb335936f50e29d419145c0e292589294e00c
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-16 18:36:54 -0700

    more UX

--- client/src/Components/DashRight.js
+++ client/src/Components/DashRight.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import avatarPlaceholder from '../Assets/placeholders/Avatar_Placeholder.png';
 import { Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 
 
 
@@ -11,6 +13,7 @@ import { Navigate } from 'react-router-dom';
 
 export default function DashRight ({ isLogout, setIsLogout, user, setUser, setPath }) {
     const [ isQuickSettings, setIsQuickSettings ] = useState(false);
+    const history = useLocation();
 
     function handleLogout() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
@@ -29,7 +32,11 @@ export default function DashRight ({ isLogout, setIsLogout, user, setUser, setPa
 
                 <h1 id={styleRightGeneric.user_header}>
                     <div id={styleRightGeneric.spacer}></div>
-                    <Link onClick={() => setPath("dashboard")} id={styleRightGeneric.username} to={`/u/${user.username}`}>&nbsp;&nbsp;{user.display_name}&nbsp;</Link>
+                    <Link onClick={() => setPath("dashboard")} id={styleRightGeneric.username} to={`/u/${user.username}`}>
+                        {history.pathname.slice(0,3) === "/u/" ?
+                        <div className={styleRightGeneric.rotate_right_90}>⌄</div>
+                        : null}
+                        {history.pathname.slice(0,3) !== "/u/" ? "⌄" : null}&nbsp;&nbsp;{user.display_name}&nbsp;</Link>
                     <div>
                         <img onClick={() => setIsQuickSettings(!isQuickSettings)} id={styleRightGeneric.dash_avatar}  src={user.avatar_url ? user.avatar_url : avatarPlaceholder}></img>
                     </div>
