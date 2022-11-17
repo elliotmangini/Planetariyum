d998e4782c0f2cb5710f2dab024811c81e5b4efc
Elliot Mangini <elliot.mangini@gmail.com>
2022-11-16 17:17:27 -0700

    some stuff

--- client/src/Components/GridItem.js
+++ client/src/Components/GridItem.js
@@ -1,17 +1,14 @@
-import style from '../StyleSheets/Home.module.css';
+import style from "../StyleSheets/Home.module.css";
 
-
-
-
-export default function GridItem ({ scrollToArticle, item }) {
-
-    return (
-                <div onClick={() => scrollToArticle(item.title)} className={item.kind === "large" ? style.grid_box_span_2 : style.grid_box}>
-                    <div className={style.publication_container}>
-                    <img className={style.cover_image} src={item.cover_url} ></img>
-                    <div className={style.publication_title}>{item.title}</div>
-                    
-                    </div>
-                </div>
-    )
+export default function GridItem({ scrollToArticle, item }) {
+  return (
+    <>
+      <div onClick={() => scrollToArticle(item.title)} className={style.grid_box}>
+        <div className={style.emptyscroll}>
+            <img className={style.cover_image} src={item.cover_url}></img>
+            <div className={style.publication_title}>{item.title}</div>
+        </div>
+      </div>
+    </>
+  );
 }
